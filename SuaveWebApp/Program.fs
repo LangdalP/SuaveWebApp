9742878057a1307cb3b30af285d7c8262dd84ffc
Peder Voldnes Langdal <pederlangdal@bekk.no>
2019-05-31 19:15:49 +0200

    Basic skeleton

---
+++ SuaveWebApp/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Suave
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    startWebServer defaultConfig (Successful.OK "Hello World!")
+    0 // return an integer exit code
